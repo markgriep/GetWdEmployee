ac5cbd999a813c55df80bda652441592e0bbd44b
megriep <markgriep@users.noreply.github.com>
2025-03-07 09:57:48 -0600

    consolidated code for the model into one file.

--- WDEmployee/Models/WorkdayEmployee.cs
+++ WDEmployee/Models/WorkdayEmployee.cs
@@ -6,6 +6,28 @@ using System.Xml.Serialization;
 namespace WDEmployee.Models
 {
 
+
+    /// <summary>
+    /// This is the ROOT of the XML response from the Workday API. In WorkDay parlance, a "report" object
+    /// </summary>
+    [XmlRoot(ElementName = "Report_Data", Namespace = "urn:com.workday.report/RandomDrugTestSelectionPool")]
+    public class WorkdayEmployeeReport
+    {
+
+        /// <summary>
+        /// These are the sub elements, individual employees in the report.  a list of WORKDAY EMPLOYEE objects
+        /// </summary>
+        [XmlElement(ElementName = "Report_Entry", Namespace = "urn:com.workday.report/RandomDrugTestSelectionPool")]
+        public List<WorkdayEmployee> Employees { get; set; } = new List<WorkdayEmployee>();
+
+    }
+
+
+
+
+    /// <summary>
+    /// One level down from the root.  Multiple EMPLOYEES
+    /// </summary>
     public class WorkdayEmployee
     {
 
